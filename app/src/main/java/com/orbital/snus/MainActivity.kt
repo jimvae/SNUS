db1bd3ccff3a1ef6394ca2524571f6866c642701
Jim Vincent <e0425954@u.nus.edu>
2020-05-24 15:07:09 +0800

    sending over to check

--- app/src/main/java/com/orbital/snus/MainActivity.kt
+++ app/src/main/java/com/orbital/snus/MainActivity.kt
@@ -2,8 +2,12 @@ package com.orbital.snus
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var signUp: Button
+    private lateinit var logIn: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
