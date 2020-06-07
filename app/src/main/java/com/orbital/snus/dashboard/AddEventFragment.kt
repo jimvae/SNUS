334864e8875e419ae48f980f5b13b3d4c01fc2f8
Jim Vincent <e0425954@u.nus.edu>
2020-06-08 01:14:33 +0800

    Update Bugs

--- app/src/main/java/com/orbital/snus/dashboard/AddEventFragment.kt
+++ app/src/main/java/com/orbital/snus/dashboard/AddEventFragment.kt
@@ -10,9 +10,11 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.orbital.snus.R
+import com.orbital.snus.databinding.ActivityDashboardBinding
 import com.orbital.snus.databinding.FragmentDashboardAddEventBinding
 import java.util.*
 
@@ -27,60 +29,57 @@ class AddEventFragment() : Fragment() {
             inflater, R.layout.fragment_dashboard_add_event, container, false
         )
 
-
         binding.textStartDate.setOnClickListener {
             val c = Calendar.getInstance()
-//            DashboardActivity().setDate(it as EditText)
-
+            setDate(it as TextView)
         }
         binding.textEndDate.setOnClickListener {
-            val c = Calendar.getInstance()
-//            DashboardActivity().setDate(it as EditText)
+            setDate(it as TextView)
         }
 
         binding.textStartTime.setOnClickListener {
-//            DashboardActivity().setTime(it as EditText)
+            setTime(it as TextView)
         }
 
         binding.textEndTime.setOnClickListener {
-//            DashboardActivity().setTime(it as EditText)
+            setTime(it as TextView)
         }
 
         binding.buttonConfirm
         return binding.root
     }
 
-//    private fun setDate(v: EditText) {
-//        val c = Calendar.getInstance()
-//        val mYear = c[Calendar.YEAR]
-//        val mMonth = c[Calendar.MONTH]
-//        val mDay = c[Calendar.DAY_OF_MONTH]
-//        val datePickerDialog = DatePickerDialog(
-//            targetFragment,
-//            DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
-//                v.setText(
-//                    dayOfMonth.toString() + "-" + (monthOfYear + 1) + "-" + year
-//                )
-//            }, mYear, mMonth, mDay
-//        )
-//        datePickerDialog.show()
-//    }
-//
-//    private fun setTime(v: EditText) {
-//        // Get Current Time
-//        val c = Calendar.getInstance()
-//        val mHour = c[Calendar.HOUR_OF_DAY]
-//        val mMinute = c[Calendar.MINUTE]
-//        val timePickerDialog = TimePickerDialog(
-//            DashboardActivity(),
-//            TimePickerDialog.OnTimeSetListener { view, hourOfDay, minute -> v.setText("$hourOfDay:$minute") },
-//            mHour,
-//            mMinute,
-//            false
-//        )
-//        timePickerDialog.show()
-//
-//    }
+    private fun setDate(v: TextView) {
+        val c = Calendar.getInstance()
+        val mYear = c[Calendar.YEAR]
+        val mMonth = c[Calendar.MONTH]
+        val mDay = c[Calendar.DAY_OF_MONTH]
+        val datePickerDialog = DatePickerDialog(
+            this.requireContext(),
+            DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+                v.setText(
+                    dayOfMonth.toString() + "-" + (monthOfYear + 1) + "-" + year
+                )
+            }, mYear, mMonth, mDay
+        )
+        datePickerDialog.show()
+    }
+
+    private fun setTime(v: TextView) {
+        // Get Current Time
+        val c = Calendar.getInstance()
+        val mHour = c[Calendar.HOUR_OF_DAY]
+        val mMinute = c[Calendar.MINUTE]
+        val timePickerDialog = TimePickerDialog(
+            this.requireContext(),
+            TimePickerDialog.OnTimeSetListener { view, hourOfDay, minute -> v.setText("$hourOfDay:$minute") },
+            mHour,
+            mMinute,
+            false
+        )
+        timePickerDialog.show()
+
+    }
 
 }
 
