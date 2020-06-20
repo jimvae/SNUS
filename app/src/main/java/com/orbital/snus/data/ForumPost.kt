38a24a00216072a70f888240735146a81053feb2
Vielheim <terencehwy@gmail.com>
2020-06-20 17:39:07 +0800

    Edit Forum data

--- app/src/main/java/com/orbital/snus/data/ForumPost.kt
+++ app/src/main/java/com/orbital/snus/data/ForumPost.kt
@@ -2,8 +2,8 @@ package com.orbital.snus.data
 
 import java.util.*
 
-data class ForumPost (val userid: String, val id: String, val date: Date, val status: Boolean,
-    val question: String, val answers: MutableList<ForumComment>) {
+data class ForumPost (val userid: String, val id: String, val title: String, val date: Date, val status: Boolean,
+    val question: String) {
     // Each ForumPost has a unique ID inside the thread
     // Add in image later
     // Add in suggested answer
