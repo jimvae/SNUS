b21611d45f5084521b9d850dac1e4d10cd449866
Jim Vincent <e0425954@u.nus.edu>
2020-06-22 15:03:51 +0800

    Review Main Page layout + fragment

---
+++ app/src/main/java/com/orbital/snus/modules/Review/MainPage/ReviewMainPageFragment.kt
@@ -0,0 +1,54 @@
+package com.orbital.snus.modules.Review.MainPageFragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.orbital.snus.R
+import com.orbital.snus.dashboard.Today.TodayEventAdapter
+import com.orbital.snus.dashboard.Today.TodayViewModel
+import com.orbital.snus.dashboard.Today.TodayViewModelFactory
+import com.orbital.snus.data.UserEvent
+import com.orbital.snus.databinding.FragmentDashboardTodayBinding
+import com.orbital.snus.databinding.ModuleForumMainPageBinding
+import java.util.ArrayList
+import java.util.Observer
+
+class ReviewMainPageFragment : Fragment() {
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var firestore: FirebaseFirestore
+
+
+    private val mods = ArrayList<String>() // holder to store events and for RecyclerViewAdapter to observe
+
+    // on main page, load out all the user's modules
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+
+        val binding: ModuleForumMainPageBinding = DataBindingUtil.inflate(
+            inflater, R.layout.module_review_main_page, container, false
+        )
+        firestore = FirebaseFirestore.getInstance()
+
+
+
+        return binding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+    }
+
+}
