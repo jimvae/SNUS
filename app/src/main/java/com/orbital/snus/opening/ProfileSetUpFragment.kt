91920e18253567fbd996de53fddc0a3c31f22f3c
Jim Vincent <e0425954@u.nus.edu>
2020-07-07 18:25:34 +0800

    Profile Setup with Backend

--- app/src/main/java/com/orbital/snus/opening/ProfileSetUpFragment.kt
+++ app/src/main/java/com/orbital/snus/opening/ProfileSetUpFragment.kt
@@ -2,20 +2,39 @@ package com.orbital.snus.opening
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.orbital.snus.R
 import com.orbital.snus.dashboard.DashboardActivity
+import com.orbital.snus.data.UserData
 import com.orbital.snus.databinding.FragmentOpeningOpeningBinding
+import com.orbital.snus.databinding.FragmentOpeningProfileSetupBinding
 
 class ProfileSetUpFragment : Fragment() {
 
     private val firebaseAuth = FirebaseAuth.getInstance()
+    private val db = FirebaseFirestore.getInstance()
+    private val firestore = FirebaseFirestore.getInstance()
+    private lateinit var binding: FragmentOpeningProfileSetupBinding
+
+    private lateinit var userData: UserData
+
+    val courses = arrayOf("Computer Science", "Business Analytics", "Computer Engineering", "Information Systems", "Information Security")
+    private lateinit var spinnerCourse: Spinner
+    private lateinit var course: String
+
+
 
 
     override fun onCreateView(
@@ -26,17 +45,92 @@ class ProfileSetUpFragment : Fragment() {
         // If logged in, connect to dashboard
 
 
-        val binding: FragmentOpeningOpeningBinding = DataBindingUtil.inflate(
+        binding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_opening_profile_setup, container, false
         )
 
-        binding.buttonLogin.setOnClickListener { view: View ->
-            view.findNavController().navigate(R.id.action_openingFragment_to_loginFragment)
-        }
 
-        binding.buttonSignup.setOnClickListener { view: View ->
-            view.findNavController().navigate(R.id.action_openingFragment_to_registerFragment)
+
+        spinnerSetup()
+
+        binding.firstLoginConfirm.setOnClickListener {
+            val bio = binding.firstLoginBio.text.toString().trim()
+            val name = binding.firstLoginName.text.toString().trim()
+            val gitHub = binding.firstLoginGithub.text.toString().trim()
+            val instagram = binding.firstLoginInstagram.text.toString().trim()
+            val linkedIn = binding.firstLoginLinkedin.text.toString().trim()
+//            val profilePhoto = binding.profilePhoto
+
+            if (TextUtils.isEmpty(name)) {
+                binding.firstLoginName.setError("Name is required")
+                return@setOnClickListener
+            }
+
+            if (TextUtils.isEmpty(bio)) {
+                binding.firstLoginBio.setError("Bio is required")
+                return@setOnClickListener
+            }
+
+            if (TextUtils.isEmpty(course)) {
+                Toast.makeText(requireContext(), "Please select your course", Toast.LENGTH_SHORT)
+                    .show()
+                return@setOnClickListener
+            }
+
+            configurePage(false)
+
+            val user = firebaseAuth.currentUser!!
+            firestore.collection("users").document(user.uid).get()
+                .addOnSuccessListener {
+                    userData = it.toObject((UserData::class.java))!!
+                }.addOnFailureListener {
+                    Toast.makeText(requireContext(), "Missing User Data", Toast.LENGTH_SHORT).show()
+                }
+//            user.upda
+
+
         }
         return binding.root
     }
+
+    fun spinnerSetup() {
+        spinnerCourse = binding.firstLoginCourseSpinner
+        spinnerCourse.adapter = ArrayAdapter(requireContext(), R.layout.module_review_spinner_layout, courses)
+
+        spinnerCourse.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                course = ""
+            }
+
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                course = courses.get(position)
+            }
+
+        }
+    }
+
+    fun configurePage(boolean: Boolean) {
+        binding.firstLoginBio.isEnabled = boolean
+        binding.firstLoginName.isEnabled = boolean
+        binding.firstLoginConfirm.isEnabled = boolean
+        binding.firstLoginCourseSpinner.isEnabled = boolean
+        binding.firstLoginGithub.isEnabled = boolean
+        binding.firstLoginInstagram.isEnabled = boolean
+        binding.firstLoginLinkedin.isEnabled = boolean
+        binding.profilePhoto.isEnabled = boolean
+    }
+
+    fun updateUserData(userData: UserData) {
+        val user = firebaseAuth.currentUser!!
+        db.collection("users") // users collection
+            .document(user.uid) // current userId
+            .set(userData)
+
+    }
+
 }
