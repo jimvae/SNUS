458959d80a1279c8f644405797cbc30880382086
Vielheim <terencehwy@gmail.com>
2020-07-26 20:32:17 +0800

    UI Fix

--- app/src/main/java/com/orbital/snus/profile/MainTimeline/MainTimelineAdapter.kt
+++ app/src/main/java/com/orbital/snus/profile/MainTimeline/MainTimelineAdapter.kt
@@ -14,6 +14,7 @@ import com.orbital.snus.modules.Forum.Posts.PostViewAdapter
 import kotlinx.android.synthetic.main.module_forum_recycler_posts.view.*
 import kotlinx.android.synthetic.main.profile_main_posts_recycler.view.*
 import java.text.SimpleDateFormat
+import java.util.*
 
 class MainTimelineAdapter (val timelineList: List<TimeLinePost>) :
     RecyclerView.Adapter<MainTimelineAdapter.TimelineViewHolder>() {
@@ -42,12 +43,34 @@ class MainTimelineAdapter (val timelineList: List<TimeLinePost>) :
         val post = timelineList[position]
         val dateFormatter: SimpleDateFormat = SimpleDateFormat("dd/MM/YYYY | hh:mm a")
         holder.textView.profile_timeline_recycler_title.text = post.title
-        holder.textView.profile_timeline_recycler_date.text =
-            dateFormatter.format(post.date!!).toPattern().toString()
+        holder.textView.profile_timeline_recycler_date.text = showDate(post.date!!)
         holder.textView.profile_timeline_recycler_extra_details.text = post.details
     }
 
     public fun getPost(position: Int) : TimeLinePost {
         return timelineList.get(position)
     }
+
+    fun showDate(given: Date) : String {
+        val dateFormatDay = SimpleDateFormat("EEEE")
+        val dateFormatDayOfYear = SimpleDateFormat("dd/MM/yyyy")
+        val dateFormatTime = SimpleDateFormat("hh:mma")
+        val weekOfYear = SimpleDateFormat("w")
+
+
+        val today = Calendar.getInstance().time
+        val thisWeek = weekOfYear.format(today).toPattern().toString()
+        val messageWeek = weekOfYear.format(given).toPattern().toString()
+
+        if (dateFormatDayOfYear.format(today).toPattern().toString().equals(dateFormatDayOfYear.format(given).toPattern().toString())) {
+            return dateFormatTime.format(given).toPattern().toString()
+
+        } else if (thisWeek.equals(messageWeek)) {
+            return dateFormatDay.format(given).toPattern().toString()
+        } else {
+            return dateFormatDayOfYear.format(given).toPattern().toString()
+        }
+
+    }
 }
+
